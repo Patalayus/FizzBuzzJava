2169539fd1f77d8b06b51de534ca6736b55c212a
Luke Charles <lukecharles222@gmail.com>
2017-09-07 17:02:44 +0100

    Initial commit

---
+++ src/fizzbuzz.java
@@ -0,0 +1,17 @@
+public class fizzbuzz {
+    public static void main(String[] args){
+        for(int i=1;i<50;i++){
+            if(i%5==0){
+                System.out.println("Buzz");
+            }
+            if(i%3==0){
+                System.out.println("Fizz");
+            }
+            if(i%5==0&&i%3==0){
+                System.out.println("FizzBuzz");
+            }else{
+                System.out.println(i);
+            }
+        }
+    }
+}
